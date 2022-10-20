3b6687d4b2f92648975c37890d714c268c760b3d
예사랑 <gkgkh12@naver.com>
2022-10-20 18:29:27 +0900

    20221020 commit

--- src/main/webapp/4_javascript_class/04_form/js/formCheck.js
+++ src/main/webapp/4_javascript_class/04_form/js/formCheck.js
@@ -1,7 +1,20 @@
 window.onload = function(){
 
 		var frm = document.getElementById('frm');
-		var inputs = document.querySelectorAll("input");
+		//var inputs = document.querySelectorAll("input");
+		
+		frm.onsubmit = function(evt){
+			evt.stopImmediatePropagation();
+			evt.preventDefault();
+			
+			//alert(frm.agree.checked);
+			if(!frm.agree.checked){
+			alert('반드시 체크해주셔야만 합니다.');
+				return;
+			}
+			
+			frm.submit();
+		}
 		
 
 
